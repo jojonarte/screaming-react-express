61c450278d94028b435ecbd2ec9df4eec9fc00cb
jojonarte <joselitonarte@gmail.com>
2020-08-29 01:56:38 +0800

    hashed password for User model

--- backendserver/auth/model.js
+++ backendserver/auth/model.js
@@ -1,22 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    minlength: 4,
-    type: String,
-    required: true,
-    select: false,
-  },
+	name: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		minlength: 4,
+		type: String,
+		required: true,
+		select: false,
+	},
+});
+
+UserSchema.pre('save', async function (next) {
+	const salt = await bcrypt.genSalt();
+	this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
