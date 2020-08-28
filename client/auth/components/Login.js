819d2dcc7e22d4e4fe04057008a641f02faeda19
jojonarte <joselitonarte@gmail.com>
2020-08-29 02:02:40 +0800

    create username and password input handlers

--- client/auth/components/Login.js
+++ client/auth/components/Login.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { requestLogin } from '../actions';
 
 function Login(props) {
-  const handleLogin = (e) => {
-    e.preventDefault();
-    props.requestLogin('jojonarte', 'jojonarte');
-  };
-  const handleSubmit = (e) => {
-    this.handleLogin(e);
-  };
-  return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <form onSubmit>
-          <button onClick={handleLogin}>Login</button>
-        </form>
-      </div>
-    </div>
-  );
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const handleLogin = (e) => {
+		e.preventDefault();
+		props.requestLogin('jojonarte', 'jojonarte');
+	};
+	const handleSubmit = (e) => {
+		this.handleLogin(e);
+	};
+	return (
+		<div>
+			<h1>Login</h1>
+			<div>
+				<form onSubmit>
+					<input
+						type="text"
+						onChange={(e) => setUsername(e.target.value)}
+						placeholder="Username"
+					></input>
+					<input
+						type="password"
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Password"
+					></input>
+					<button onClick={handleLogin}>Login</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 const __mapDispatchToProps = {
-  requestLogin,
+	requestLogin,
 };
 export default connect(null, __mapDispatchToProps)(Login);
