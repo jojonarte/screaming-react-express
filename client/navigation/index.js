2c9fa379f606939c68be5dcc93f9643b888e05eb
jojonarte <joselitonarte@gmail.com>
2020-08-30 20:22:43 +0800

    nav css

--- client/navigation/index.js
+++ client/navigation/index.js
@@ -4,16 +4,16 @@ import { Dashboard } from '../dashboard';
 import { connect } from 'react-redux';
 
 function Navigation() {
-  return (
-    <div>
-      <Link to="/dashboard">
-        <h1>Dashboard</h1>
-      </Link>
-      <Link to="/login">
-        <h1>Login</h1>
-      </Link>
-    </div>
-  );
+	return (
+		<nav className="nav">
+			<Link className="nav-link" to="/dashboard">
+				<h1>Dashboard</h1>
+			</Link>
+			<Link className="nav-link" to="/login">
+				<h1>Login</h1>
+			</Link>
+		</nav>
+	);
 }
 
 export default connect()(Navigation);
