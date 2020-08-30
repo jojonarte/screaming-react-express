3d0708d75ca66116189a0a6535e463d597daaad6
jojonarte <joselitonarte@gmail.com>
2020-08-30 21:57:33 +0800

    refactor root routes

--- client/app/index.js
+++ client/app/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import history from '../store/history';
-import Router from './router';
+import RootRoute from './routes';
 
 export default function App() {
 	return (
 		<BrowserRouter history={history}>
 			<Provider store={store}>
-				<Route component={Router} />
+				<Route component={RootRoute} />
 			</Provider>
 		</BrowserRouter>
 	);
